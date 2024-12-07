e2dbac0ee61c221cd2022180a1077201ab2c1305
mariia-ty <tymoshenkom@icloud.com>
2024-12-07 17:36:57 -0500

    finished

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 //assign 1
 const txtArea = document.querySelector(".txtArea");
-txtArea.style.marginTop = "20px";
-
 const btn = document.querySelector(".btn");
+btn.textContent = "Click Me";
 
-btn.textContent = txtArea;
+btn.addEventListener("click", () => {
+  btn.textContent = txtArea.value;
+});
+
+//assign 2
+const image = document.querySelector(".image");
+image.style.width = "700px";
+image.style.height = "400px";
+image.src = "https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg";
+
+//assign 3
+const img2 = document.querySelector(".img2");
+img2.style.width = "700px";
+img2.style.height = "400px";
+const link = document.querySelector(".link");
+img2.alt = "Picture of nature";
+link.href =
+  "https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg";
+
+//assign 4
+const item = document.querySelector(".item");
+item.textContent = "Blue";
